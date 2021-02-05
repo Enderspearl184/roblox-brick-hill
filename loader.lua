379be50a37eb6ce3afbf4439f02ba5e22ac6d3ef
Enderspearl184 <73859349+Enderspearl184@users.noreply.github.com>
2021-02-04 21:08:09 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,9 @@
 --dummy score is located in the leaderstats folder of their model.
-_G.differentServer=false --if you are hosting this on a different computer than what you are testing this in studio, set this to True
+--incase your version of the roblox place is before i added the loader, add this to serverscriptservice and remove all the other scripts and guis and stuff
+_G.differentServer=false --set to true only if testing on studio using a different device than what you are hosting on
 _G.port=42481 --use this for the port of the server
 _G.url="urlherelol" --set this to the url or ip the server is running on
-_G.UseModel=false --keep this to true
+_G.UseModel=true --keep this to true, setting it to false may cause bugs as it uses whatever your version has, instead of downloading the model
 
 
 local scriptfolder
