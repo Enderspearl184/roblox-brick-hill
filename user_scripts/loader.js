9e5581380e1a3d1206254d47c22e683fa59ba739
Enderspearl184 <73859349+Enderspearl184@users.noreply.github.com>
2021-02-04 18:43:47 -0800

    Debug (#3)
    * Update loader.js
    
    * Update README.md
    
    * Update README.md (#1)
    
    * Debug (#2)
    
    * Update README.md
    
    * Update FakePlayers.js
    
    * Update FakePlayers.js

--- user_scripts/loader.js
+++ user_scripts/loader.js
@@ -1,9 +1,11 @@
 Game.rbxport=42481; //change this to the second port you forwarded
 
+const branch="main" //set to either main or debug (main is default)
+
 let phin = getModule("phin")
 
-const fakeplayerurl="https://raw.githubusercontent.com/Enderspearl184/roblox-brick-hill/main/source/FakePlayers.js"
-const hitdetectionurl="https://raw.githubusercontent.com/Enderspearl184/roblox-brick-hill/main/source/brickHitDetection.js"
+const fakeplayerurl=`https://raw.githubusercontent.com/Enderspearl184/roblox-brick-hill/${branch}/source/FakePlayers.js`
+const hitdetectionurl=`https://raw.githubusercontent.com/Enderspearl184/roblox-brick-hill/${branch}/source/brickHitDetection.js`
 
 function load() {
 	loadurl(fakeplayerurl)
